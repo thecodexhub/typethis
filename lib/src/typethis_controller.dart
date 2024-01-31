735edff5934a364ea503eb416f70126edc8ae9de
Sandip <sandipramanik07@gmail.com>
2024-01-31 22:54:57 +0530

    feat: revamp controller and rewrite typethis logic

--- lib/src/typethis_controller.dart
+++ lib/src/typethis_controller.dart
@@ -1,104 +1,40 @@
-import 'dart:async';
+import 'package:flutter/widgets.dart';
 
-import 'package:flutter/foundation.dart';
-
-/// {@template typethis_controller}
-/// A controller for the [TypeThis] widget.
-///
-/// It facilitates different operations on the typing animation.
-/// For example, the `reset()` method resets the typing animation,
-/// and restarts it from the beginning.
-///
-/// Remember to [dispose] of the [TypeThisController] when it is no longer
-/// needed. This will ensure we discard any resources used by the object.
-/// {@endtemplate}
-class TypeThisController with ChangeNotifier {
-  // Track the steps
-  int _steps = 0;
-
-  // Getter to track the step. Each step means adding a character
-  // to the typing animation.
-  int get steps => _steps;
-
-  // Duration on which [Timer.periodic] will run.
-  final Duration _periodicDuration;
-
-  // Length of the string
-  final int _maxBoundLength;
-
-  // Tracks the timer to reset and cancel when dispose.
-  @visibleForTesting
-  Timer? timer;
-
-  /// {@macro typethis_controller}
-  TypeThisController(Duration duration, int maxBoundLength)
-      : _periodicDuration = duration,
-        _maxBoundLength = maxBoundLength {
-    _startTimerAndNotify();
-  }
-
-  // Starts the timer and notifies the listener as a callback.
-  void _startTimerAndNotify() {
-    timer = Timer.periodic(_periodicDuration, (_) {
-      _steps++;
-      notifyListeners();
-
-      if (_steps == _maxBoundLength) {
-        timer?.cancel();
-      }
-    });
-  }
+/// Controller for managing or controlling the state of the typing animation.
+class TypeThisController extends ChangeNotifier {
+  TypeThisControllerState _state = TypeThisControllerState.start;
+  /// The current state of the typing animation.
+  TypeThisControllerState get state => _state;
 
   /// Resets the typing animation and restarts it from beginning.
-  ///
-  /// ```dart
-  /// // Extract the [TypeThis] widget into a separate variable.
-  /// final typeThisWidget = TypeThis(
-  ///   string: 'Hi there! How are you doing?',
-  ///   speed: 50,
-  ///   style: TextStyle(fontSize: 18, color: Colors.black),
-  /// );
-  ///
-  /// // Resets the animation
-  /// typeThisWidget.controller.reset();
-  /// ```
   void reset() {
-    timer?.cancel();
-    _steps = 0;
-    _startTimerAndNotify();
+    _state = TypeThisControllerState.start;
+    notifyListeners();
   }
 
   /// Freezes the typing animation.
-  ///
-  /// ```dart
-  /// // Extract the [TypeThis] widget into a separate variable.
-  /// final typeThisWidget = TypeThis(
-  ///   string: 'Hi there! How are you doing?',
-  ///   speed: 50,
-  ///   style: TextStyle(fontSize: 18, color: Colors.black),
-  /// );
-  ///
-  /// // Freezes the animation
-  /// typeThisWidget.controller.freeze();
-  /// ```
   void freeze() {
-    timer?.cancel();
+    _state = TypeThisControllerState.frozen;
+    notifyListeners();
   }
 
-  /// Unfreezes the typing animation, means the animation resumes
+  /// Unfreezes or resumes the typing animation, means the animation resumes
   /// from where it was frozen last time.
   void unfreeze() {
-    // for safety, if unfreeze() is called without freeze() being called
-    // previously.
-    timer?.cancel();
-    if (_steps != _maxBoundLength) {
-      _startTimerAndNotify();
-    }
+    _state = TypeThisControllerState.resumed;
+    notifyListeners();
   }
+}
 
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
-  }
+/// Enum representing the possible states of the typing animation.
+enum TypeThisControllerState {
+  /// The initial state, where the typing animation begins.
+  /// Also, when the animation restarts.
+  start,
+
+  /// State when the typing animation has paused.
+  frozen,
+
+  /// State when the typing animation has resumed.
+  resumed
 }
